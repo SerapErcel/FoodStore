d7c3f4616e543074c9758987d8d77902aee380b9
Serap <ercelserap@gmail.com>
2022-10-24 15:51:09 +0300

    Add menu and search

--- app/src/main/java/com/serapercel/foodstore/ui/fragment/DetailFragment.kt
+++ app/src/main/java/com/serapercel/foodstore/ui/fragment/DetailFragment.kt
@@ -1,22 +1,49 @@
 package com.serapercel.foodstore.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.serapercel.foodstore.R
+import com.serapercel.foodstore.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
 
         val bundle:DetailFragmentArgs by navArgs()
         val tFood = bundle.food
 
+        binding.toolbarDetail.title = tFood.yemek_adi
 
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarDetail)
+
+        requireActivity().addMenuProvider(object :MenuProvider{
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_cart_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when(menuItem.itemId){
+                    R.id.action_detail_cart -> {
+                        Navigation.findNavController(binding.toolbarDetail).navigate(R.id.goToCartFromDetail)
+                        Toast.makeText(requireContext(), "click on cartFragment", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+        },viewLifecycleOwner,Lifecycle.State.RESUMED)
+
+        return binding.root
     }
 }
--- app/src/main/java/com/serapercel/foodstore/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/serapercel/foodstore/ui/fragment/HomeFragment.kt
@@ -1,16 +1,66 @@
 package com.serapercel.foodstore.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
 import com.serapercel.foodstore.R
+import com.serapercel.foodstore.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container, false)
+        binding.toolbarHome.title = "Foods"
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarHome)
+
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_search_menu, menu)
+
+                val item = menu.findItem(R.id.action_search)
+                val searchView = item.actionView as SearchView
+                searchView.setOnQueryTextListener(this@HomeFragment)
+            }
+
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                return when (item.itemId) {
+                    R.id.action_cart -> {
+                        Navigation.findNavController(binding.toolbarHome).navigate(R.id.goToCart)
+                        Toast.makeText(requireContext(), "click on cartFragment", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_search -> {
+                        Toast.makeText(requireContext(), "click on search", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        return binding.root
     }
+
+    override fun onQueryTextSubmit(query: String): Boolean {
+        Toast.makeText(requireContext(), "click on onQueryTextSubmit", Toast.LENGTH_SHORT).show()
+
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String): Boolean {
+        Toast.makeText(requireContext(), "click on onQueryTextChange", Toast.LENGTH_SHORT).show()
+
+        return true
+    }
+
+
 }
