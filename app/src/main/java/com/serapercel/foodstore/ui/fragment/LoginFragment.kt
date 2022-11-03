51372159a27ca19d3a54ac2e8fcf02e180d060b6
Serap <ercelserap@gmail.com>
2022-11-04 01:34:29 +0300

    Add login design

---
+++ app/src/main/java/com/serapercel/foodstore/ui/fragment/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.serapercel.foodstore.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serapercel.foodstore.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
